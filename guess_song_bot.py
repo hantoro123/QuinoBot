40d977c326f45133aaf58b0ba9260f121d488f2e
SangJun Han <grw05177@naver.com>
2023-07-25 20:51:28 +0900

    QuinoBot 간단한 명령어 입출력 테스트 설정

--- guess_song_bot.py
+++ guess_song_bot.py
@@ -8,8 +8,48 @@ CHANNEL_ID = 'Channel ID'
 class MyClient(discord.Client):
     async def on_ready(self):
         channel = self.get_channel(CHANNEL_ID)
-        await channel.send('안녕 퀴노봇이야 뭘 도와줄까?')
+        await channel.send('안녕 난 퀴노봇이야 뭘 도와줄까?')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        answer = self.get_answer(message.content)
+        await message.channel.send(answer)
+
+    def get_day_of_week(self):
+        weekday_list = ['월','화','수','목','금','토','일']
+
+        weekday = weekday_list[datetime.today().weekday()]
+        date = datetime.today().strftime("%Y년 %m월 %d일")
+        result = '{}({})'.format(date,weekday)
+        return result
+    
+    def get_answer(self, text):
+        trim_text = text.replace(" ", "")
  
+        answer_dict = {
+            '안녕': '안녕하세요. QuinoBot입니다.',
+            '요일': ':calendar: 오늘은 {}입니다'.format(self.get_day_of_week()),
+            '시간': ':clock9: 현재 시간은 {}입니다.'.format(self.get_time()),
+        }
+ 
+        if trim_text == '' or None:
+            return "알 수 없는 질의입니다. 답변을 드릴 수 없습니다."
+        elif trim_text in answer_dict.keys():
+            return answer_dict[trim_text]
+        else:
+            for key in answer_dict.keys():
+                if key.find(trim_text) != -1:
+                    return "연관 단어 [" + key + "]에 대한 답변입니다.\n" + answer_dict[key]
+ 
+            for key in answer_dict.keys():
+                if answer_dict[key].find(text[1:]) != -1:
+                    return "질문과 가장 유사한 질문 [" + key + "]에 대한 답변이에요.\n" + answer_dict[key]
+ 
+        return text + "은(는) 없는 질문입니다."
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
